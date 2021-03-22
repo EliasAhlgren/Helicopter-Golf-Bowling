5b267949933ad5583debf980ffd9e5a53a884e9f
EliasAhlgren <elias.ahlgrem.ea@gmail.com>
2021-03-22 10:32:28 +0200

    xassd

---
+++ New Unity Project/Assets/Scripts/GameManagement/Menu/HostButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GameManagement.Menu
+{
+    public class HostButton : MonoBehaviour
+    {
+        void StartServer()
+        {
+            PlayerPrefs.SetInt("ShouldStartClient", 1);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/GameManagement/Menu/InputField.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputField : MonoBehaviour
+{
+
+    private TMP_InputField _inputField;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputField = gameObject.GetComponent<TMP_InputField>();
+    }
+
+    void AttemptJoin()
+    {
+        PlayerPrefs.SetInt("ShouldStartClient", 1);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        _inputField.characterValidation = TMP_InputField.CharacterValidation.Decimal;
+        PlayerPrefs.SetString("HostIp", _inputField.text);
+    }
+}
